17a4838e192af0a8ede8989a160ed43e026d1beb
xiangmujingli <1334614764@qq.com>
2020-09-15 11:48:23 +0800

    555

--- src/testgit/testone.java
+++ src/testgit/testone.java
@@ -3,10 +3,10 @@ package testgit;
 public class testone {
 
 	public static void main(String[] args) {
-		System.out.println("111");
-		System.out.println("222");
-		System.out.println("444");
+		System.out.println("1");
+		System.out.println("22");
+		System.out.println("4444");
+		System.out.println("55555");
 	}
-
 }
- 
+    
